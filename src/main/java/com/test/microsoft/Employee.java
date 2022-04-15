85c1d3e3f3ae73257dc535b503dcb8b2b47d3901
Ron <ronniedelarosa@hotmail.com>
2022-04-15 14:18:51 -0400

    ways resolvingemployee percentage

--- src/main/java/com/test/microsoft/Employee.java
+++ src/main/java/com/test/microsoft/Employee.java
@@ -2,19 +2,55 @@ package com.test.microsoft;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class Employee {
+public class Employee implements Comparable<Employee> {
     private String name;
     private String jobTitle;
+
+    private List<Employee> directReport = new ArrayList<>();
 
     public Employee(String name, String jobTitle) {
         this.name = name;
         this.jobTitle = jobTitle;
     }
 
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public List<Employee> getDirectReport() {
+        return directReport;
+    }
+
+    public void setDirectReport(Employee emp) {
+        this.directReport.add(emp);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getJobTitle() {
         return jobTitle;
     }
 
-    public List<Employee> directReport = new ArrayList<>();
+    public Stream<Employee> flattened() {
+        return Stream.concat(
+                Stream.of(this),
+                directReport.stream().flatMap(Employee::flattened));
+    }
+
+    public Long size() {
+        return flattened().count();
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return this.jobTitle.compareTo(o.jobTitle);
+    }
 }
--- src/main/java/com/test/microsoft/ExamMsApp.java
+++ src/main/java/com/test/microsoft/ExamMsApp.java
@@ -4,35 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ExamMsApp {
+
+    private Employee getEmployee() {
+        Employee ceo = new Employee("Kevin", "CEO");
+
+        Employee man1 = new Employee("Mike", "Manager");
+        man1.setDirectReport(new Employee("Lori", "Graphics"));
+
+        Employee man2 = new Employee("Jon", "Manager");
+        man2.setDirectReport(new Employee("Jerry", "SWE"));
+
+        ceo.setDirectReport(man1);
+        ceo.setDirectReport(man2);
+
+        return ceo;
+    }
+
     /**
      * Given ceo has many employees what's the percentage per each job
      */
     public static void example() {
         ExamMsApp examMsApp = new ExamMsApp();
 
-        Employee ceo = new Employee("Kevin", "CEO");
+        Employee ceo = examMsApp.getEmployee();
 
-        Employee man1 = new Employee("Mike", "Manager");
-        man1.directReport.add(new Employee("Lori", "Graphics"));
+        // number of ways to solve employee percentage
+//        List<JobTitleInfo> results = examMsApp.bruteForce(ceo);
+//
+//        List<JobTitleInfo> results = examMsApp.stream1(ceo);
+//
+//        List<JobTitleInfo> results = examMsApp.stream2(ceo);
+//
+        List<JobTitleInfo> results = examMsApp.embeddedInModel(ceo);
 
-        Employee man2 = new Employee("Jon", "Manager");
-        man2.directReport.add(new Employee("Jerry", "SWE"));
-
-        ceo.directReport.add(man1);
-        ceo.directReport.add(man2);
-
-        examMsApp.getJobTitlePercentage(ceo)
-                .forEach(j -> System.out.println(" job title: " + j.getJobTitle() + ", percentage: " + j.getPercentage() + "%"));
-
+        for (JobTitleInfo j : results) {
+            System.out.println("Job " + j.getJobTitle() + ", counts: " + j.getPercentage() + "%");
+        }
     }
 
-    private int employeeCountByJobTitle(Employee emp, Map<String, Integer> groupJob) {
+    private double calcPercentage(Long titleCounts, Long employeeSize) {
+        double result = ((double)titleCounts) / employeeSize;
+        return (int) (result * 100);
+    }
 
+    private List<JobTitleInfo> stream2(Employee ceo) {
+        System.out.println("attached flattened, size: " + ceo.size());
+
+        // only reporting to CEO
+        return ceo.flattened()
+                .filter(f -> !f.getJobTitle()
+                        .equalsIgnoreCase("ceo"))
+                .collect(Collectors.groupingBy(Employee::getJobTitle, TreeMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(f -> new JobTitleInfo(f.getKey(),calcPercentage(f.getValue(),(ceo.size() - 1))))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Good start for using the stream
+     */
+    private List<JobTitleInfo> embeddedInModel(Employee emp) {
+        TreeMap<String, Long> grouping = emp.flattened()
+                .filter(f -> !f.getJobTitle().equalsIgnoreCase("ceo"))
+                .collect(Collectors.groupingBy(Employee::getJobTitle, TreeMap::new, Collectors.counting()));
+
+        return grouping.entrySet()
+                .stream()
+                .map(f -> new JobTitleInfo(f.getKey(),calcPercentage(f.getValue(),(emp.size() - 1))))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Option 1: Brute force
+     */
+    private int employeeCountByJobTitle(Employee emp, Map<String, Integer> groupJob) {
         int count = 0;
 
-        for (Employee e : emp.directReport) {
+        for (Employee e : emp.getDirectReport()) {
             if (groupJob.containsKey(e.getJobTitle())) {
                 groupJob.put(e.getJobTitle(), groupJob.get(e.getJobTitle()) + 1);
             } else {
@@ -41,21 +95,57 @@ public class ExamMsApp {
 
             count++;
 
-            if (e.directReport.size() > 0)
+            if (e.getDirectReport().size() > 0)
                 count += employeeCountByJobTitle(e, groupJob);
         }
 
         return count;
     }
 
+    private List<JobTitleInfo> bruteForce(Employee emp) {
+        List<JobTitleInfo> results = new ArrayList<>();
+
+        Map<String, Integer> groupJob = new TreeMap<>();
+        int employeeSize = employeeCountByJobTitle(emp, groupJob);
+
+        System.out.println("employees: " + employeeSize);
+
+        for (Map.Entry<String, Integer> set : groupJob.entrySet()) {
+            // when used tow integer it will become 0.0 instead of like 0.25 due to rounding to nearest
+            double percent = ((double) set.getValue() / employeeSize) * 100;
+            results.add(new JobTitleInfo(set.getKey(), (int)percent));
+        }
+
+        return results;
+    }
+
+    public static Stream<Employee> flatten(Employee emp) {
+        return Stream.concat(
+                Stream.of(emp),
+                emp.getDirectReport().stream().flatMap(ExamMsApp::flatten));
+//                .peek(System.out::println);
+    }
+
+    /**
+     * Simple method of using stream
+     */
+    private List<JobTitleInfo> stream1(Employee emp) {
+        List<Employee> flattened = emp.getDirectReport().stream()
+                .flatMap(ExamMsApp::flatten)
+                .collect(Collectors.toList());
+
+        System.out.println("flattened helper, size: " + emp.size());
+        return flattened
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getJobTitle, TreeMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(f -> new JobTitleInfo(f.getKey(),calcPercentage(f.getValue(),(emp.size() - 1))))
+                .collect(Collectors.toList());
+    }
 
     private List<JobTitleInfo> getJobTitlePercentage(Employee ceo) {
         List<JobTitleInfo> results = new ArrayList<>();
-        // only good for not recursive
-        //  Map<String, Long> employeeCountByJobTitle =
-//        ceo.directReport.stream()
-//                .collect(Collectors.groupingBy(Employee::getJobTitle, TreeMap::new, Collectors.counting()))
-//                .forEach((jobTitle, count) -> results.add(new JobTitleInfo(jobTitle, count)));
 
         Map<String, Integer> groupJob = new TreeMap<>();
         int employeeSize = employeeCountByJobTitle(ceo, groupJob);
@@ -65,7 +155,7 @@ public class ExamMsApp {
         for (Map.Entry<String, Integer> set : groupJob.entrySet()) {
             // when used tow integer it will become 0.0 instead of like 0.25 due to rounding to nearest
             double percent = ((double) set.getValue() / employeeSize) * 100;
-            results.add(new JobTitleInfo(set.getKey(), percent));
+            results.add(new JobTitleInfo(set.getKey(), (int)percent));
         }
 
         return results;
